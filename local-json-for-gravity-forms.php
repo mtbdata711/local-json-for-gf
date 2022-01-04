e153da2f3ce4d8de2243a06f4b7d9d9888b7a0cd
Matthew Benge <bengethomasmatthew@gmail.com>
2022-01-04 20:20:30 +0000

    Update out of date comment

--- local-json-for-gravity-forms.php
+++ local-json-for-gravity-forms.php
@@ -3,11 +3,6 @@
 /**
  * The plugin bootstrap file
  *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
  * @link              https://github.com/mtbdata711/local-json-for-gf
  * @since             1.0.0
  * @package           Local_JSON_for_Gravity_Forms
